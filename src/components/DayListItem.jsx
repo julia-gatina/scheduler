193cdb7a16408363c3d886bcbbfb0bcda44a81b7
Julia <julia.sarandi@gmail.com>
2022-01-19 19:46:55 -0800

    Create file with tests for DayItemList, add function to assign message for spots remaining

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 import 'components/DayListItem.scss';
 
+const formatSpots = (spots) => {
+  let spotsString = '';
+
+  if (spots === 0) {
+    spotsString = 'no spots';
+  } else if (spots === 1) {
+    spotsString = '1 spot';
+  } else {
+    spotsString = `${spots} spots`;
+  }
+  return spotsString;
+};
+
 export default function DayListItem(props) {
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
@@ -12,7 +25,7 @@ export default function DayListItem(props) {
       <h2 className='day' onClick={props.setDay}>
         {props.name}
       </h2>
-      <h3 className='text--light'>{props.spots} spots remaining</h3>
+      <h3 className='text--light'>{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
