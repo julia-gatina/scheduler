c1a04bd20e87e0239a33ec873af2a7c5465db97b
Julia <julia.sarandi@gmail.com>
2022-01-19 18:39:24 -0800

    Add props to DaylistItem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function DayListItem(props) {
   return (
     <li>
-      <h2 className='text--regular'>Day Name</h2>
-      <h3 className='text--light'>X spots remaining</h3>
+      <h2 className='text--regular' onClick={props.setDay}>
+        {props.name}
+      </h2>
+      <h3 className='text--light'>{props.spots} spots remaining</h3>
     </li>
   );
 }
