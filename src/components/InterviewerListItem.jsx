8395374b9f35f6693e8146782bf860bc304d3ac4
Julia <julia.sarandi@gmail.com>
2022-01-20 09:29:22 -0800

    Add classes conditionally for InterviewerListItem

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import 'components/InterviewerListItem.scss';
+import classNames from "classnames";
 
 
 const InterviewerListItem = (props) => {
+  const interviewerClass = classNames ("interviewers__item", {
+    "--selected": props.selected
+  });
   return (
-    <li className="interviewers__item" onClick={props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={props.setInterviewer(props.id)}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
