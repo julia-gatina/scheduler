a544d880ce2d10164a63a82bf9a38d2d0e026a8b
Julia <julia.sarandi@gmail.com>
2022-01-20 14:03:12 -0800

    Add dynamically rendered InterviewerListItems to InterviewerList

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -3,10 +3,24 @@ import InterviewerListItem from "./InterviewerListItem";
 import 'components/InterviewerList.scss'
 
 export default function InterviewerList(props) {
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const dynamicInterviewerListItem = interviewers.map((singleInterviewer) => {
+    return (
+      <InterviewerListItem 
+      key={singleInterviewer.id}
+      id={singleInterviewer.id}
+      name={singleInterviewer.name}
+      avatar={singleInterviewer.avatar}
+      selected={singleInterviewer.id === interviewer}
+      />
+    )
+  })
+
   return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+    <section className="interviewers" onClick={setInterviewer}>
+      <h4 className="interviewers__header text--light" >Interviewer</h4>
+      <ul className="interviewers__list" >{dynamicInterviewerListItem}</ul>
     </section>
   );
 };
