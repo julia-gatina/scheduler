2f750e6df3e330e099f822dd1a40cfa3fecac8db
Julia <julia.sarandi@gmail.com>
2022-01-19 19:08:10 -0800

    Add scss file and conditional styles for DayListItem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import classNames from 'classnames';
+import 'components/DayListItem.scss';
 
 export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.full,
+  });
   return (
-    <li>
-      <h2 className='text--regular' onClick={props.setDay}>
+    <li className={dayClass}>
+      <h2 className='day' onClick={props.setDay}>
         {props.name}
       </h2>
       <h3 className='text--light'>{props.spots} spots remaining</h3>
--- stories/index.jsx
+++ stories/index.jsx
@@ -28,7 +28,7 @@ storiesOf('DayListItem', module) //Initiates Storybook and registers our DayList
   }) // Provides the default background color for our component
   .add('Unselected', () => <DayListItem name='Monday' spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
   .add('Selected', () => <DayListItem name='Monday' spots={5} selected />)
-  .add('Full', () => <DayListItem name='Monday' spots={0} />)
+  .add('Full', () => <DayListItem name='Monday' spots={0} full />)
   .add('Clickable', () => (
     <DayListItem name='Tuesday' setDay={action('setDay')} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
