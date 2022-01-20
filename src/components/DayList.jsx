405414c19b9847e64c1c2b11cae43a78d8fc5740
Julia <julia.sarandi@gmail.com>
2022-01-20 15:23:23 -0800

    Refactor Daylist to use 'value' and 'on change'

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 const DayList = (props) => {
-  const { days, day, setDay } = props;
   const dynamicDayListItem = days.map((singleDay) => {
     return (
       <DayListItem
-        key={singleDay.id}
-        name={singleDay.name}
-        spots={singleDay.spots}
-        selected={singleDay.name === day}
-        setDay={setDay(singleDay.id)}
-      />
+      key={singleDay.id} 
+      name={singleDay.name} 
+      spots={singleDay.spots} 
+      selected={singleDay.name === props.value}
+      setDay={singleDay.onChange}
+     />
     );
   });
   return <ul> {dynamicDayListItem} </ul>;
