23f3ce9c3fa9b73737ff11573e3de796ead19697
Julia <julia.sarandi@gmail.com>
2022-01-20 16:47:02 -0800

    ADd Empty component, story and onClick action

--- stories/index.jsx
+++ stories/index.jsx
@@ -10,6 +10,8 @@ import InterviewerListItem from 'components/InterviewerListItem'
 import InterviewerList from 'components/InterviewerList';
 import Appointment from "components/Appointment/index.jsx";
 import Header from 'components/Appointment/Header'
+import Empty from 'components/Appointment/Empty'
+
 
 
 
@@ -140,3 +142,5 @@ storiesOf("DayList", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => (<Empty onAdd={action("onAdd")}/>))
+  
