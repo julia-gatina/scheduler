dcebc49d43c0f42a16631edfb3021e91aee1613e
Julia <julia.sarandi@gmail.com>
2022-01-21 22:06:26 -0800

    Add Appointment Empty and Appointment Booked stories, add logic to render Show and Empty components conditionally

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react'
 import './styles.scss';
+import Show from "./Show";
+import Header from "./Header";
+import Empty from "./Empty";
 
 const Appointment = (props) => {
-  return (
-    <article className="appointment" time={props.time}>
-      
-    </article>
-  );
+    const {time, id, interview } = props;
+    return (
+        <article className="appointment" id={id} time={time}>
+            <Header time={time}/>
+            {interview ?
+                <Show student={interview.student} interviewer={interview.interviewer.name}/> : <Empty/>}
+        </article>
+    );
 };
 
 export default Appointment;
