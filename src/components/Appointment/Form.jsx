a466f70d837a5fd3f52494aeee565c56dc258878
Julia <julia.sarandi@gmail.com>
2022-01-21 20:59:28 -0800

    Update Form component to change student name value and select interviewer using useState, pass props

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,41 +1,49 @@
-import React from "react"
+import React, {useState} from "react"
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
-import { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
     const [student, setStudent] = useState(props.student || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
-  return (
-      <main className="appointment__card appointment__card--create">
-        <section className="appointment__card-left">
-          <form autoComplete="off">
-            <input
-                className="appointment__create-input text--semi-bold"
-                name="name"
-                type="text"
-                placeholder="Enter Student Name"
-                onChange={setStudent}
-                value={student}
-                /*
-                  This must be a controlled component
-                  your code goes here
-                */
-            />
-          </form>
-          <InterviewerList
-              /* your code goes here */
-          />
-        </section>
-        <section className="appointment__card-right">
-          <section className="appointment__actions">
-            <Button danger {/* your code goes here */}>Cancel</Button>
-            <Button confirm {/* your code goes here */}>Save</Button>
-          </section>
-        </section>
-      </main>
-  );
+    const studentChanged = (event) => {
+        setStudent(event.target.value);
+    };
+
+    const reset = () => {
+
+    };
+    const cancel = () => {
+        reset()
+    };
+
+    return (
+        <main className="appointment__card appointment__card--create">
+            <section className="appointment__card-left">
+                <form autoComplete="off">
+                    <input
+                        className="appointment__create-input text--semi-bold"
+                        name="name"
+                        type="text"
+                        placeholder="Enter Student Name"
+                        onChange={studentChanged}
+                        value={student}
+                    />
+                </form>
+                <InterviewerList
+                    interviewers={props.interviewers}
+                    onChange={setInterviewer}
+                    selected={interviewer}
+                />
+            </section>
+            <section className="appointment__card-right">
+                <section className="appointment__actions">
+                    <Button danger onClick={props.onCancel}>Cancel</Button>
+                    <Button confirm onClick={props.onSave}>Save</Button>
+                </section>
+            </section>
+        </main>
+    );
 };
 
 export default Form;
--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -4,23 +4,23 @@ import 'components/InterviewerList.scss'
 
 export default function InterviewerList(props) {
 
-  const dynamicInterviewerListItem = props.interviewers.map((interviewer) => {
-    return (
-      <InterviewerListItem 
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={interviewer.id === props.value}
-        setInterviewer={() => props.onChange(interviewer.id)}
-      />
-    )
-  })
+    const dynamicInterviewerListItem = props.interviewers.map((interviewer) => {
+        return (
+            <InterviewerListItem
+                key={interviewer.id}
+                name={interviewer.name}
+                avatar={interviewer.avatar}
+                selected={interviewer.id === props.selected}
+                setInterviewer={() => props.onChange(interviewer.id)}
+            />
+        )
+    })
 
-  return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light" >Interviewer</h4>
-      <ul className="interviewers__list">{dynamicInterviewerListItem}</ul>
-    </section>
-  );
+    return (
+        <section className="interviewers">
+            <h4 className="interviewers__header text--light">Interviewer</h4>
+            <ul className="interviewers__list">{dynamicInterviewerListItem}</ul>
+        </section>
+    );
 };
 
