30cbfb38d529ca7768b62bf27b6bd7c85494ed04
Julia <julia.sarandi@gmail.com>
2022-01-22 12:42:10 -0800

    Install and import axios

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,25 +1,8 @@
-import React, {useState} from "react";
+import React, {useState}, {axios} from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
 const appointments = [
   {
     id: 1,
@@ -61,6 +44,7 @@ const appointments = [
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
 
   return (
     <main className="layout">
