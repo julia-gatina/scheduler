640cb8db1c711d15f4163110608a5f492fff6174
Julia <julia.sarandi@gmail.com>
2022-01-22 23:28:47 -0800

    Implement getAppointmentsForDay function

--- src/helpers/selectors.jsx
+++ src/helpers/selectors.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+export function getAppointmentsForDay(state, day) {
+  const dayArray = state.days.filter((item) => item.name === day);
+  if (dayArray.length === 0) {
+    return [];
+  }
+  const arrayOfAppointmentsToFind = dayArray[0].appointments;
+  const appointmentsArrayToRender = arrayOfAppointmentsToFind.map((apptId) => state.appointments[apptId]);
+  return appointmentsArrayToRender;
+};
