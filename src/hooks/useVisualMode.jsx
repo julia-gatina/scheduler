4a8d84bff40aea668d41bcd3af7ddcdd61a4c704
Julia <julia.sarandi@gmail.com>
2022-01-25 14:25:34 -0800

    Optimize code to not mutate original history array

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -1,4 +1,3 @@
-
 import {useState} from "react";
 
 const useVisualMode = (initial, replace) => {
@@ -6,24 +5,27 @@ const useVisualMode = (initial, replace) => {
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-    if (!replace) {
-      setMode(newMode);
-      history.push(newMode);
-      setHistory(history);
+    const newHistory = [...history];
+    if (replace) {
+      newHistory.pop();
     }
+
     setMode(newMode);
-    history.pop()
-    history.push(newMode);
-    setHistory(history);
-  }
-  function back() {
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
-    }
+    newHistory.push(newMode);
+    setHistory(newHistory);
   }
 
-  return { mode, transition, back };
+  function back() {
+    if (history.length < 2) {
+      return;
+    }
+    const newHistory = [...history];
+    newHistory.pop();
+    setHistory(newHistory);
+    setMode(newHistory[newHistory.length - 1]);
+  }
+
+  return {mode, transition, back};
 };
 
 export default useVisualMode;
